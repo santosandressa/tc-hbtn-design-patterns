13506f2017079538f913d2d2ed3fd27fb6632937
santosandressa <andressasantosp0@gmail.com>
2022-07-07 15:55:36 -0300

    task 0

--- 0x02/lanchonete/Pedido.java
+++ 0x02/lanchonete/Pedido.java
@@ -15,15 +15,15 @@ public class Pedido {
 
     @Override
     public String toString() {
-        String foraDaCaixa = "\tFora da Caixa:\n";
+        StringBuilder foraDaCaixa = new StringBuilder("\tFora da Caixa:\n");
 
         for (ItemPedido item : itensForaCaixa) {
-            foraDaCaixa += String.format("\t\t- %s %s\n", item.getTipo(), item.getNome());
+            foraDaCaixa.append(String.format("\t\t- %s %s\n", item.getTipo(), item.getNome()));
         }
-        String dentroDaCaixa = "\tDentro da Caixa:\n";
+        StringBuilder dentroDaCaixa = new StringBuilder("\tDentro da Caixa:\n");
         for (ItemPedido item : itensDentroCaixa) {
-            dentroDaCaixa += String.format("\t\t- %s %s\n", item.getTipo(), item.getNome());
+            dentroDaCaixa.append(String.format("\t\t- %s %s\n", item.getTipo(), item.getNome()));
         }
-        return foraDaCaixa + dentroDaCaixa;
+        return foraDaCaixa + dentroDaCaixa.toString();
     }
 }
